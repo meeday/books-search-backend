d2ff721573ccc2354271fbdc678ce3c5d93aac1b
meeday <59993824+meeday@users.noreply.github.com>
2020-08-29 11:40:05 +0100

    cors added

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const cors = require("cors");
 
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 
 
